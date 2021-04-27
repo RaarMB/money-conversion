b686c3f1bf977abd426d88063710225e58161492
Raquel Martinez <raquelmartinez@MacBook-Pro-de-Raquel.local>
2021-04-26 22:41:38 -0500

    Fix | remove icon textInputLayout and add imageview for flags, update strings instructions, enabled button and progress bar |Raquel Martinez

--- app/src/main/java/com/moneyconversion/home/HomeConversionViewModel.kt
+++ app/src/main/java/com/moneyconversion/home/HomeConversionViewModel.kt
@@ -2,7 +2,10 @@ package com.moneyconversion.home
 
 import android.content.Context
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.viewModelScope
 import com.moneyconversion.model.Money
 import com.moneyconversion.network.MoneyConversionRepository
 import com.moneyconversion.network.MoneyConversionRepository.Companion.MXN_CODE
@@ -20,6 +23,11 @@ class HomeConversionViewModel @Inject constructor(
     private val context: Context
 ) : ViewModel() {
 
+    private val moneyList = listOf(
+        Money(MXN_CODE, ContextCompat.getDrawable(context, R.drawable.ic_mxn_flag), true),
+        Money(USD_CODE, ContextCompat.getDrawable(context, R.drawable.ic_usd_flag), false)
+    )
+
     private var moniesFrom = listOf(
         Money(MXN_CODE, ContextCompat.getDrawable(context, R.drawable.ic_mxn_flag), true),
         Money(USD_CODE, ContextCompat.getDrawable(context, R.drawable.ic_usd_flag), false)
@@ -35,7 +43,8 @@ class HomeConversionViewModel @Inject constructor(
     fun getMoniesTo()= MutableLiveData<List<Money>>().apply { value = moniesTo }
 
     val amount = MutableLiveData<String>()
-    var conversionResult = MutableLiveData<String>()
+    val conversionResult = MutableLiveData<String>()
+    val showProgress = MutableLiveData<Boolean>()
 
     private val selectedMoneyFrom = MediatorLiveData<Money>().apply {
         addSource(getMoniesFrom()) { monies ->
@@ -58,29 +67,19 @@ class HomeConversionViewModel @Inject constructor(
     }
 
     fun selectedMoneyFrom(money: Money) {
-        val moniesFromUpdate = moniesFrom.map {
-            it.copy(selected = it.id == money.id)
-        }
-        val moniesToUpdate = moniesTo.map {
-            it.copy(selected = it.id != money.id)
-        }
-        selectedMoneyFrom.value = moniesFromUpdate.first { it.selected }
-        selectedMoneyTo.value = moniesToUpdate.first { it.selected }
+        selectedMoneyFrom.value = money
+        selectedMoneyTo.value = moneyList.first { it.id != money.id }.copy(selected = money.selected)
     }
 
     fun selectedMoneyTo(money: Money) {
-        val moniesFromUpdate = moniesFrom.map {
-            it.copy(selected = it.id != money.id)
-        }
-        val moniesToUpdate = moniesTo.map {
-            it.copy(selected = it.id == money.id)
-        }
-        selectedMoneyTo.value = moniesToUpdate.first { it.selected }
-        selectedMoneyFrom.value = moniesFromUpdate.first { it.selected }
+        selectedMoneyTo.value = money
+        selectedMoneyFrom.value = moneyList.first { it.id != money.id }.copy(selected = money.selected)
     }
 
     fun conversion() {
+        conversionResult.value = ""
         viewModelScope.launch {
+            showProgress.value = true
             try {
                 val response = repository.getConversion(
                     selectedMoneyFrom.value?.id.toString(),
@@ -93,18 +92,19 @@ class HomeConversionViewModel @Inject constructor(
                 }
             } catch (exception: Exception) {
                 conversionResult.value = exception.message.toString()
+            } finally {
+                showProgress.value = false
             }
         }
     }
 
-    val isConversionEnabled: LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+    val isConversionEnabled = MediatorLiveData<Boolean>().apply {
         addSource(amount) { value = validateInformation() }
         addSource(selectedMoneyItemFrom) { value = validateInformation() }
         addSource(selectedMoneyItemTo) { value = validateInformation() }
-        addSource(conversionResult) { value = validateInformation() }
     }
 
     private fun validateInformation() =
         amount.value != null && selectedMoneyItemFrom.value != null &&
-                selectedMoneyItemTo.value != null && conversionResult.value != null
+                selectedMoneyItemTo.value != null
 }
